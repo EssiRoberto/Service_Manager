e0b17ad3f3ad687ee50162a513faadc247658755
EssiRoberto <essiroberto@gmail.com>
2026-08-24 15:47:36 +0100

    new commit

--- file_io.c
+++ file_io.c
@@ -7,5 +7,6 @@ int main(void) {
         fclose(file);
         printf("Wrote PID to file\n");
     }
+    return 0;
 
 }
--- fork_test.c
+++ fork_test.c
@@ -4,7 +4,7 @@
 int main(void) {
     pid_t pid = fork();
     if (pid == 0) {
-        printf("I am the child process!\n");	    
+        printf("I am the child process\n");	    
     }else if (pid > 0) {
 	printf("I am the parent process\n");    
     }
