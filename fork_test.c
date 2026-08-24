b5e5131aaed34404ad4e0b4456973d94cea5eb5e
EssiRoberto <essiroberto@gmail.com>
2026-08-24 16:03:00 +0100

    Fix fork formatting

--- fork_test.c
+++ fork_test.c
@@ -4,10 +4,9 @@
 int main(void) {
     pid_t pid = fork();
     if (pid == 0) {
-        printf("I am the child process\n");	    
+        printf("I am the child process\n");
     } else if (pid > 0) {
-	printf("I am the parent process\n");    
+        printf("I am the parent process\n");
     }
-
     return 0;
-}
+}i
